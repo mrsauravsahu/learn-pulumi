539c878a7db5c27aee43d12ef95ef0c8dcd6ca41
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-18 19:58:39 +0530

    add tag to storage account

--- src/basics-vm/index.ts
+++ src/basics-vm/index.ts
@@ -2,14 +2,17 @@ import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
 
 // Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup("resourceGroup");
+const resourceGroup = new azure.core.ResourceGroup("basics-vm");
 
 // Create an Azure resource (Storage Account)
-const account = new azure.storage.Account("storage", {
+const account = new azure.storage.Account("storageprimary", {
     // The location for the storage account will be derived automatically from the resource group.
     resourceGroupName: resourceGroup.name,
     accountTier: "Standard",
     accountReplicationType: "LRS",
+    tags: {
+        "Stage": "Dev"
+    }
 });
 
 // Export the connection string for the storage account
